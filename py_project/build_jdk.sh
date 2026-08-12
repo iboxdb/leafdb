d5fae90c079bcb8a9aa95b57b9f8fdb9d84a4b51
Bruce <iboxdb@users.noreply.github.com>
2026-08-12 12:38:03 -0400

    py

---
+++ py_project/build_jdk.sh
@@ -0,0 +1,11 @@
+
+
+
+# https://github.com/moditect/moditect
+# https://deepwiki.com/moditect/moditect
+
+
+mvn clean package exec:java
+
+./target/jlink-image/bin/java --list-modules
+
---
+++ py_project/src/main/java/benchmark/BenchmarkDBTest.java
@@ -0,0 +1,19 @@
+package benchmark;
+
+
+import iboxdb.localserver.*;
+import iboxdb.localserver.io.*;
+import java.io.*;
+
+public class BenchmarkDBTest {
+ 
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("jlink " + DB.class);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
