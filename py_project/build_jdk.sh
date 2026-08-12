cca3d66a7a4dc630ee39dd0babb11c373fc4e375
Bruce <iboxdb@users.noreply.github.com>
2026-08-12 15:12:08 -0400

    ok

--- py_project/build_jdk.sh
+++ py_project/build_jdk.sh
@@ -1,11 +1,17 @@
 
 
-
 # https://github.com/moditect/moditect
-# https://deepwiki.com/moditect/moditect
+# [user@localhost site-packages]$ jdeps -s  org.jpype.jar 
 
 
 mvn clean package exec:java
 
 ./target/jlink-image/bin/java --list-modules
+
+./target/jlink-image/bin/java -cp ./target/jlink-image/jars/linkjdk-1.0.jar benchmark.BenchmarkDBTest
+
+python jb.py
+
+
+
 
---
+++ py_project/jb.py
@@ -0,0 +1,57 @@
+print("iBoxDB Java Python!")
+
+# pip install jpype1
+# Download openjdk
+
+import os;
+print(os.environ.get("JAVA_HOME"))
+os.environ["JAVA_HOME"] = "./target/jlink-image/"
+print(os.environ.get("JAVA_HOME"))
+
+
+import jpype
+# ModuleNotFoundError if not imported
+import jpype.imports
+ 
+ 
+print("startJVM...");
+jpype.startJVM()
+
+print("JVM: ", jpype.getDefaultJVMPath())
+print("Ver: ", jpype.getJVMVersion())
+
+#jlink jdk don't need to load this.
+#jpype.addClassPath("iboxdb-4.1.2.jar")
+
+from java.lang import Long, Double, String
+from java.math import BigDecimal          
+from iboxdb.localserver import Ason,DB
+
+
+print("CRUD",Ason.class_,DB.class_)
+
+# Prototype
+proto = Ason("id:",Long(0), "name:",String("_"), "val:",Double(0.0), "star", BigDecimal('0'))
+
+DB.root("../DBRoot")
+db = DB(1)
+cfg = db.getConfig()
+cfg.ensureTable(proto,"table","id")
+isUnique = False
+cfg.ensureIndex(proto,"table",isUnique,"name")
+auto = db.open()
+
+print(dir(auto))
+print(dir(proto))
+print(proto.clone.__doc__)
+
+
+
+
+
+
+#------------------------#
+auto.getDatabase().close()
+jpype.shutdownJVM()
+
+del jpype
